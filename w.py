80da5631dc1dccf52dcdba3227fecdabfcffd684
Prudhvi Boyapalli <prb2@rice.edu>
2015-05-14 19:50:28 -0500

    Switched to Fahrenheit.

--- w.py
+++ w.py
@@ -12,7 +12,7 @@ if (len(sys.argv) > 1):
 
 	base = "https://query.yahooapis.com/v1/public/yql?"
 
-	query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + city + '" ) and u="c"'
+	query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + city + '" ) and u="f"'
 
 	# query = "select wind from weather.forecast where woeid=2460286"
 
@@ -37,7 +37,7 @@ if (len(sys.argv) > 1):
 	
 	if (len(sys.argv) == 2):
 		current = channel["item"]["condition"]
-		print "* Currently " + current["temp"] + temp +  ", " + current["text"]
+		print "* Currently " + current["temp"] + " " + temp +  ", " + current["text"]
 	else :
 		option = sys.argv[2]
 		forecast = channel["item"]["forecast"]
@@ -45,24 +45,24 @@ if (len(sys.argv) > 1):
 		if (option == "today"):
 			today = forecast[0]
 			print "* " + today["day"] + " " + today["date"]
-			print "* " + today["text"] + ", with a high of " + today["high"] + temp + " and a low of " + today["low"] + temp
+			print "* " + today["text"] + ", with a high of " + today["high"] +  " " + temp + " and a low of " + today["low"] +  " " + temp
 
 		elif (option == "tomorrow"):
 			tomorrow = forecast[1]
 			print "* " + tomorrow["day"] + " " + tomorrow["date"]
-			print "* " + tomorrow["text"] + ", with a high of " + tomorrow["high"] + temp + " and a low of " + tomorrow["low"] + temp
+			print "* " + tomorrow["text"] + ", with a high of " + tomorrow["high"] + " " +  temp + " and a low of " + tomorrow["low"] + " " + temp
 		
 		elif (option == "week"):
 				for day in forecast:
 					print "* " + day["day"] + " " + day["date"]
-					print "* " + day["text"] + ", with a high of " + day["high"] + temp + " and a low of " + day["low"] + temp
+					print "* " + day["text"] + ", with a high of " + day["high"] +  " " + temp + " and a low of " + day["low"] +  " " + temp
 					if (day != forecast[len(forecast)-1]):
 							print "* "
 
 		elif (option == "wind"):
 			wind = channel["wind"]
 			print "* ~ Wind ~"
-			print "* Chill:     " + wind["chill"] + temp
+			print "* Chill:     " + wind["chill"] +  " " + temp
 			print "* Direction: " + wind["direction"] + " degrees"
 			print "* Speed:     " + wind["speed"] + speed
 		
